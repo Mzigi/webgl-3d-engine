c5189255c48de6416002dd4fa73ec5d66c37ddf2
Mzigi <smrengen@gmail.com>
2023-05-09 18:28:13 +0200

    detect when a key started being presse

--- engine/input.js
+++ engine/input.js
@@ -10,10 +10,30 @@ var PressedKeys = {
     "ArrowLeft": false,
     " ": false,
     "Shift": false,
+    "e": false,
+    "c": false,
+    "o": false,
+    "n": false,
+    "l": false,
+}
+
+var PressedKeysStart = {
+    "w" : false,
+    "a" : false,
+    "s" : false,
+    "d" : false,
+    "ArrowUp": false,
+    "ArrowRight": false,
+    "ArrowDown": false,
+    "ArrowLeft": false,
+    " ": false,
+    "Shift": false,
+    "e": false,
 }
 
 document.body.onkeydown = function(e) {
     PressedKeys[e.key] = true
+    PressedKeysStart[e.key] = true
 }
 
 document.body.onkeyup = function(e) {
@@ -25,5 +45,29 @@ function isKeyPressed(keycode) {
         return true
     } else {
         return false
+    }
+}
+
+function keyPressStarted(keycode) {
+    if (PressedKeysStart[keycode]) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function resetInputs() {
+    PressedKeysStart = {
+        "w" : false,
+        "a" : false,
+        "s" : false,
+        "d" : false,
+        "ArrowUp": false,
+        "ArrowRight": false,
+        "ArrowDown": false,
+        "ArrowLeft": false,
+        " ": false,
+        "Shift": false,
+        "e": false,
     }
 }
